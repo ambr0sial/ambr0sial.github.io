bf778c5bd5895c46906e798d829262dfd0c539e2
waw <81994421+ambr0sial@users.noreply.github.com>
2023-11-05 22:51:35 +0100

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,106 @@
+// particles-config.js
+particlesJS('particles', {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: '#c89efc'
+        },
+        shape: {
+            type: 'circle',
+            stroke: {
+                width: 2,
+                color: '#000000'
+            },
+            polygon: {
+                nb_sides: 5
+            },
+        },
+        opacity: {
+            value: 0.7,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 2,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#c89efc',
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: {
+                enable: true,
+                mode: 'repulse'
+            },
+            onclick: {
+                enable: true,
+                mode: 'push'
+            },
+            resize: true
+        },
+        modes: {
+            repulse: {
+                distance: 100,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            }
+        }
+    },
+    retina_detect: true
+});
+
+function redirectToAsh() {
+    window.location.href = 'https://ash.ambrosial.fun';
+}
+
+function animateOToZero() {
+    const oElement = document.getElementById('changing-o');
+    const text = oElement.innerText;
+
+    if (text === 'o') {
+        oElement.innerText = '0';
+    } else {
+        oElement.innerText = 'o';
+    }
+}
+
+setInterval(animateOToZero, 1000);
